75c837825ea34b4c50752857bd9bbf197bffd102
Mohammad Jafari <jafarim@gmail.com>
2019-02-09 18:36:59 -0800

    support for wildcard and arrays in granted scopes.

--- pauldron-hearth/lib/PermissionEvaluation.js
+++ pauldron-hearth/lib/PermissionEvaluation.js
@@ -1,14 +1,35 @@
 const _ = require("lodash");
 
-function evaluateRequestedScopeAgainstGrantedScope(grantedScope, requesterScope) {
-    return _.isEqual(grantedScope, requesterScope) ? "Permit" : "NotApplicable";
+function wildCardAndArrayMatcher (grantedScopeValue, requestedScopeValue) { 
+    if (grantedScopeValue==="*") 
+        return true; 
+    else if (Array.isArray(grantedScopeValue)) {
+        //todo: this needs a check to make sure there's a cap on the number of cascaded arrays to keep this recursion under control and curb out of memory attacks.
+        return Array.isArray(requestedScopeValue) 
+            ? requestedScopeValue.every((requestedScopeValueElement) =>
+                    grantedScopeValue.some((grantedScopeValueElement)=> 
+                        _.isEqualWith(grantedScopeValueElement, requestedScopeValueElement, wildCardAndArrayMatcher)
+                    )
+              )
+            : grantedScopeValue.some((grantedScopeValueElement) => 
+                _.isEqualWith(grantedScopeValueElement, requestedScopeValue, wildCardAndArrayMatcher)
+              );
+    }
+    //no else. Otherwise, returns undefined which triggers isEqualWith to fall back to using deepEqual.
+}
+
+function evaluateRequestedScopeAgainstGrantedScope(grantedScope, requestedScope) {
+    const denied = grantedScope.deny;
+    const matched = _.isEqualWith(_.omit(grantedScope, ["deny"]), requestedScope, wildCardAndArrayMatcher);
+    return matched 
+        ? (denied ? "Deny" : "Permit") 
+        : "NotApplicable";
 }
 
 function evaluateRequestedScopeAgainstGrantedScopes(grantedScopes, requestedScope) {
-    const evaluationResults = grantedScopes.map(
-        (grantedScope) => evaluateRequestedScopeAgainstGrantedScope(grantedScope, requestedScope)
-    );
-    
+    const evaluationResults = grantedScopes.map((grantedScope) => 
+        evaluateRequestedScopeAgainstGrantedScope(grantedScope, requestedScope)
+    );    
     return !evaluationResults.some((result)=>(result === "Deny")) 
             && evaluationResults.some((result)=>(result === "Permit"));
 }
